e751f60c487321a1118f7e0c87d709cb1b97e635
MPLine <michelp6090@gmail.com>
2025-10-01 16:48:48 -0700

    update setting

--- lorhanj/settings.py
+++ lorhanj/settings.py
@@ -131,3 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CSRF_TRUSTED_ORIGINS = ['127.0.0.1','lorhanj.onrender.com']
